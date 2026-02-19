2e040fd461910ae82ef52ecc37c6974e97c7644c
charbelbc <charbel.bouchaaya@oulu.fi>
2026-02-19 14:28:51 +0200

    added job

---
+++ job.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --account=project_2007401
+#SBATCH --job-name=testdreamer
+#SBATCH --partition=gpu
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=32
+#SBATCH --mem-per-cpu=8000
+#SBATCH --time=50:00:00
+#SBATCH --gres=gpu:v100:1
+#SBATCH --output=logs/wireless.out
+#SBATCH --error=logs/wireless.err
+
+module --force purge
+module load pytorch/2.2
+source /projappl/project_2007401/genflownet/bin/activate
+cd /projappl/project_2007401/AAA
+
+srun python main.py
