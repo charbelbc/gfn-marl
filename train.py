fix normalization

train.py
@@ -111,34 +111,43 @@ def train(
 
 
 class RunningMeanStd:
-    def __init__(self, shape):
-        self.n = 0
-        self.mean = np.zeros(shape)
-        self.S = np.zeros(shape)
-        self.std = np.sqrt(self.S)
+    def __init__(self, shape=()):
+        self.mean = np.zeros(shape, dtype=np.float64)
+        self.var = np.ones(shape, dtype=np.float64)
+        self.count = 0  # small value to avoid division issues
 
     def update(self, x):
-        x = np.array(x)
-        self.n += 1
-        if self.n == 1:
-            self.mean = x
-            self.std = x
-        else:
-            old_mean = self.mean.copy()
-            self.mean = old_mean + (x - old_mean) / self.n
-            self.S = self.S + (x - old_mean) * (x - self.mean)
-            self.std = np.sqrt(self.S / self.n)
+        x = np.asarray(x, dtype=np.float64)
+        batch_mean = np.mean(x, axis=0)
+        batch_var = np.var(x, axis=0)
+        batch_count = x.shape[0]
+        self._update_from_moments(batch_mean, batch_var, batch_count)
+
+    def _update_from_moments(self, batch_mean, batch_var, batch_count):
+        delta = batch_mean - self.mean
+        total_count = self.count + batch_count
+        new_mean = self.mean + delta * batch_count / total_count
+        m_a = self.var * self.count
+        m_b = batch_var * batch_count
+        M2 = m_a + m_b + delta**2 * self.count * batch_count / total_count
+        new_var = M2 / total_count
+        self.mean = new_mean
+        self.var = new_var
+        self.count = total_count
+
+    @property
+    def std(self):
+        return np.sqrt(self.var + 1e-8)
 
 
 class Normalization:
-    def __init__(self, shape):
-        self.running_ms = RunningMeanStd(shape=shape)
+    def __init__(self, shape=()):
+        self.running_ms = RunningMeanStd(shape)
 
     def __call__(self, x, update=True):
         if update:
             self.running_ms.update(x)
-        x = (x - self.running_ms.mean) / (self.running_ms.std + 1e-8)
-        return x
+        return (x - self.running_ms.mean) / self.running_ms.std
 
 
 def train_mpe(